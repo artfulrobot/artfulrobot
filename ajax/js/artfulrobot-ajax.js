203bf1937a2f94e9954774f9b14dfe2343484964
Rich Lott <rich.lott@peopleandplanet.org>
2013-02-15 23:42:50 +0000

    added artfulrobot.queryString as pre-parsed query string

--- ajax/js/artfulrobot-ajax.js
+++ ajax/js/artfulrobot-ajax.js
@@ -349,6 +349,21 @@ artfulrobot.Exception = artfulrobot.defineClass( {/*{{{*/
 		return msg;
 	}
 });/*}}}*/
+
+// from http://stackoverflow.com/a/3855394/623519
+artfulrobot.queryString = //{{{
+(function(a) { 
+     if (a == "") return {};
+     var b = {};
+     for (var i = 0; i < a.length; ++i)
+     {
+     var p=a[i].split('=');
+     if (p.length != 2) continue;
+     b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+     }
+     return b;
+     })(window.location.search.substr(1).split('&')); // }}}
+
 /* functions for making forms easier */
 artfulrobot.getRadioValue = function( radioGroupName ) // {{{
 {
