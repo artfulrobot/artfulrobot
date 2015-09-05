bf05213dd3f0b6dbd9b1c9a26d4f3875b44f9415
Rich Lott <forums@artfulrobot.uk>
2015-09-05 08:31:27 +0100

    Validate - add __get, and notEmpty and more tess.

--- ArtfulRobot/ValidateItem.php
+++ ArtfulRobot/ValidateItem.php
@@ -23,11 +23,31 @@ class ValidateItem {
       $this->value = $this->validate->input($key);
     }
   }
-  public function required() {
-    if ($this->value === null) {
-      throw new \InvalidArgumentException("$this->key missing.");
+  public function __invoke() {
+    return $this->__get('v');
+  }
+  public function __get($prop) {
+    if (empty($this->value) && $this->allow_empty) {
+      $_ = $this->empty_value;
     }
-    return $this;
+    else {
+      $_ = $this->value;
+    }
+    if ($prop == 'v' || $prop == 'value') {
+      return $_;
+    }
+    elseif ($prop == 'string') {
+      return (string)$_;
+    }
+    elseif ($prop == 'int') {
+      return (int)$_;
+    }
+    elseif ($prop == 'bool') {
+      return (bool) $_;;
+    }
+  }
+  public function __tostring() {
+    return $this->__get('string');
   }
   /**
    * Ignore cast validations if empty, and set the value returned.
@@ -37,42 +57,6 @@ class ValidateItem {
   public function allowEmpty($empty_value = null) {
     $this->allow_empty = TRUE;
     $this->empty_value = $empty_value;
-    return $this;
-  }
-  /**
-   * If the value is empty, set it to this default.
-   */
-  public function defaultIfEmpty($default) {
-    if (empty($this->value)) {
-      $this->set($default);
-    }
-    return $this;
-  }
-  /**
-   * If the value is missing, set it to this default.
-   *
-   * This is more specific than defaultIfEmpty.
-   */
-  public function defaultIfMissing($default) {
-    if ($this->value === null) {
-      $this->set($default);
-    }
-    return $this;
-  }
-  public function __invoke() {
-    return $this->get();
-  }
-  public function get() {
-    if (empty($this->value) && $this->allow_empty) {
-      return $this->empty_value;
-    }
-    return $this->value;
-  }
-  public function __tostring() {
-    return (string) $this->get();
-  }
-  public function set($value) {
-    $this->value = $value;
     return $this;
   }
   public function castToDate($format='Y-m-d') {
@@ -97,6 +81,84 @@ class ValidateItem {
   public function castToBool() {
     $_ = (bool) $this->value;
     $this->value = $_;
+    return $this;
+  }
+  /**
+   * If the value is empty, set it to this default.
+   */
+  public function defaultIfEmpty($default) {
+    if (empty($this->value)) {
+      $this->set($default);
+    }
+    return $this;
+  }
+  /**
+   * If the value is missing, set it to this default.
+   *
+   * This is more specific than defaultIfEmpty.
+   */
+  public function defaultIfMissing($default) {
+    if ($this->value === null) {
+      $this->set($default);
+    }
+    return $this;
+  }
+  /**
+   * Ensure value matches.
+   *
+   * $valid can be:
+   * - regexp
+   * - array of strings
+   */
+  public function matches($valid) {
+    if ($this->allow_empty && empty($this->value)) {
+      return $this;
+    }
+    if (is_string($valid)) {
+      if (!preg_match($valid, $this->value)) {
+        throw new \InvalidArgumentException("$this->key is not as expected");
+      }
+    }
+    elseif (is_array($valid)) {
+      if (!in_array($this->value, $valid)) {
+        throw new \InvalidArgumentException("$this->key is not one of the allowed values.");
+      }
+    }
+    return $this;
+  }
+  /**
+   * We must have *something* for this item.
+   *
+   * - Any non-empty value passed in input
+   * - Value set through a defaultIfMissing
+   *
+   * It makes no sense to do allowEmpty()->notEmpty(). allowEmpty is ignored
+   * and notEmpty will still throw exception.
+   */
+  public function notEmpty() {
+    if (empty($this->value)) {
+      throw new \InvalidArgumentException("$this->key required.");
+    }
+    return $this;
+  }
+  /**
+   * We must have a value for this item.
+   *
+   * - Any value passed in input
+   * - Value set through a defaultIfMissing
+   * - 'empty' value set through allowEmpty (this can still be 'null')
+   */
+  public function required() {
+    if ($this->allow_empty && empty($this->value)) {
+      return $this;
+    }
+    if ($this->value === null) {
+      throw new \InvalidArgumentException("$this->key missing.");
+    }
+    return $this;
+  }
+  public function set($value) {
+    $this->value = $value;
     return $this;
   }
 }
--- tests/ValidateTest.php
+++ tests/ValidateTest.php
@@ -18,10 +18,10 @@ class ValidateTest extends \PHPUnit_Framework_TestCase {
    */
   public function testDefaults() {
     $v = new Validate(['id'=>'']);
-    $this->assertEquals('foo', $v->aMissingField->defaultIfMissing('foo')->get());
-    $this->assertEquals('foo', $v->anotherMissingField->defaultIfEmpty('foo')->get());
-    $this->assertEquals('', $v->id->defaultIfMissing('foo')->get());
-    $this->assertEquals('foo', $v->id->defaultIfEmpty('foo')->get());
+    $this->assertEquals('foo', $v->aMissingField->defaultIfMissing('foo')->v);
+    $this->assertEquals('foo', $v->anotherMissingField->defaultIfEmpty('foo')->v);
+    $this->assertEquals('', $v->id->defaultIfMissing('foo')->v);
+    $this->assertEquals('foo', $v->id->defaultIfEmpty('foo')->v);
   }
   /**
    * @expectedException \InvalidArgumentException
@@ -88,7 +88,7 @@ class ValidateTest extends \PHPUnit_Framework_TestCase {
     $v = new Validate(['date'=>'']);
     $this->assertEquals('today', (string) $v->date->allowEmpty('today'));
     $this->assertEquals('', (string) $v->foo);
-    $this->assertNull($v->foo->get());
+    $this->assertNull($v->foo->v);
   }
   /**
    * @expectedException \InvalidArgumentException
@@ -105,6 +105,125 @@ class ValidateTest extends \PHPUnit_Framework_TestCase {
   public function testIntWithString() {
     $v = new Validate(['id'=>'foo']);
     $v->id->castToInt(1);
+  }
+  /**
+   * @expectedException \InvalidArgumentException
+   * @expectedExceptionMessage id is not one of the allowed values.
+   */
+  public function testPatternArrayFail() {
+    $v = new Validate(['id'=>'foo']);
+    $v->id->matches(['bar', 'bad']);
+  }
+  public function testPatternArrayPass() {
+    $v = new Validate(['id'=>'foo']);
+    $v->id->matches(['bar', 'bad', 'foo']);
+  }
+  /**
+   * @expectedException \InvalidArgumentException
+   * @expectedExceptionMessage id is not as expected
+   */
+  public function testPatternRegexFail() {
+    $v = new Validate(['id'=>'foo']);
+    $v->id->matches('/^goo/');
+  }
+  public function testPatternRegexPass() {
+    $v = new Validate(['id'=>'foo']);
+    $v->id->matches('/^foo/');
+  }
+  public function testPropCasting() {
+    $v = new Validate(['id'=>'45','check'=>'checked', 'date'=>'today', 'i'=>10]);
+    $this->assertInternalType('int', $v->i->int);
+    $this->assertEquals(10, $v->i->int);
+    $this->assertInternalType('string', $v->i->string);
+    $this->assertEquals('10', $v->i->string);
+
+    $this->assertInternalType('int', $v->id->int);
+    $this->assertEquals(45, $v->id->int);
+    $this->assertInternalType('string', $v->id->value);
+    $this->assertEquals(45, $v->id->value);
+    $v->id->castToInt();
+    $this->assertInternalType('int', $v->id->value);
+    $this->assertEquals(45, $v->id->value);
+    $this->assertInternalType('int', $v->id->int);
+    $this->assertEquals(45, $v->id->int);
+    $this->assertInternalType('string', $v->id->string);
+    $this->assertEquals('45', $v->id->string);
+
+    $this->assertInternalType('bool', $v->misingcheckbox->bool);
+    $this->assertEquals(FALSE, $v->misingcheckbox->bool);
+    $this->assertInternalType('bool', $v->check->bool);
+    $this->assertEquals(TRUE, $v->check->bool);
+  }
+  /**
+   * the required() method throws an exception if the value is missing.
+   *
+   * We should be able to satisfy required() by setting a default value,
+   * and by telling it to allowEmpty.
+   */
+  public function testRequiredHappyAfterDefault() {
+    $v = new Validate(['zls' => '']);
+    $this->assertInstanceOf('\ArtfulRobot\ValidateItem',
+      $v->foo->defaultIfMissing('bar')->required());
+
+    $this->assertInstanceOf('\ArtfulRobot\ValidateItem',
+      $v->bar->allowEmpty()->required());
+
+    // ZLS is valid
+    $this->assertInstanceOf('\ArtfulRobot\ValidateItem',
+      $v->zls->required());
+  }
+  /**
+   * the notEmpty() method throws an exception if the value is empty.
+   *
+   * @expectedException \InvalidArgumentException
+   * @expectedExceptionMessage foo required
+   */
+  public function testNotEmptyFailsIfMissing() {
+    $v = new Validate(['zls' => '']);
+    $v->foo->notEmpty();
+  }
+  /**
+   * the notEmpty() method throws an exception if the value is empty.
+   *
+   * @expectedException \InvalidArgumentException
+   * @expectedExceptionMessage zls required
+   */
+  public function testNotEmptyFailsIfZls() {
+    $v = new Validate(['zls' => '']);
+    $v->zls->notEmpty();
+  }
+  /**
+   * the notEmpty() method throws an exception if the value is empty - inc 0.
+   *
+   * This is slightly odd, but it's the way PHP'e empty() works, so we stick
+   * with it.
+   *
+   * @expectedException \InvalidArgumentException
+   * @expectedExceptionMessage zero required
+   */
+  public function testNotEmptyFailsIfZero() {
+    $v = new Validate(['zero'=>0]);
+    $v->zero->notEmpty();
+  }
+  /**
+   * the notEmpty() method throws an exception if the value is empty.
+   *
+   * We should be able to satisfy required() by setting a default value,
+   * but not by telling it to allowEmpty.
+   *
+   * @expectedException \InvalidArgumentException
+   * @expectedExceptionMessage bar required.
+   */
+  public function testNotEmpty() {
+    $v = new Validate(['zls' => '']);
+
+    // setting default should be Ok
+    $this->assertInstanceOf('\ArtfulRobot\ValidateItem',
+      $v->foo->defaultIfMissing('bar')->notEmpty());
+
+    // this should throw exception.
+    $this->assertInstanceOf('\ArtfulRobot\ValidateItem',
+      $v->bar->allowEmpty()->notEmpty());
   }
 }
 
