dae6e9d331452cb4a2fa1011fc99419505f5c3f8
Rich Lott <forums@artfulrobot.uk>
2016-08-19 12:15:30 +0100

    HtmlScrape now has useful stuff in it

--- ArtfulRobot/HtmlScrape.php
+++ ArtfulRobot/HtmlScrape.php
@@ -30,6 +30,13 @@ class HtmlScrape {
    */
   public $xpath;
 
+  /** DOMElement as cache of main section identified by getMainSection.
+   */
+  public $mainSectionNode;
+  /**
+   * @var bool Whether the content has been loaded.
+   */
+  public $content_loaded = FALSE;
   /**
    * Convenience factory method.
    */
@@ -40,24 +47,47 @@ class HtmlScrape {
   /**
    * Constructor.
    *
-   * @param string $url URL to load.
+   * @param NULL|string $url URL to load.
    */
   public function __construct($url=NULL) {
+    if ($url) {
+      $this->loadFromUrl($url);
+    }
+  }
+  /**
+   * Initialise with URL.
+   */
+  public function loadFromUrl($url) {
     $this->url = $url;
-    $this->raw = file_get_contents($url);
-    $this->dom = new \DOMDocument();
-    $this->dom->loadHTML($this->raw);
-    $this->xpath = new \DOMXPath($this->dom);
+    $this->content_loaded = FALSE;
+    $this->xpath = NULL;
+    $this->dom = NULL;
+  }
+  /**
+   * Initialise with HTML string.
+   *
+   * Useful for testing.
+   */
+  public function loadFromString($html) {
+    $this->raw = $html;
+    $this->parseRaw();
+    return $this;
   }
   /**
    * Return a URL to a representative image.
    */
   public function getImageUrl() {
+    $this->requireContents();
 
     // Try the og:image.
-    $image = $this->getOgProperty('og:image');
-    if ($image) {
+    if ($image = $this->getOgProperty('og:image')) {
       return $image;
+    }
+
+    // Try to fetch first image from main section.
+    $nodes = $this->xpath->evaluate("//img", $this->getMainSection());
+    if ($nodes->length) {
+      return $nodes->item(0)->getAttribute('src');
     }
 
   }
@@ -65,18 +95,42 @@ class HtmlScrape {
    * Get HTML title.
    */
   public function getTitle() {
+    $this->requireContents();
     return $this->getTextContentFromXPath('//html/head/title');
   }
   /**
    * Get a description.
+   *
+   * Return the first available description:
+   *
+   * 1. meta description
+   * 2. meta og:description
    */
   public function getDescription() {
+    $this->requireContents();
 
-    $nodes = $this->xpath->evaluate("//html/head/meta[@name='description']");
-    if ($nodes->length > 0 && $nodes->item(0)->hasAttribute('content')) {
-      return $nodes->item(0)->getAttribute('content');
+    if ($_ = $this->getMetaByName('description')) {
+      return $_;
+    }
+    if ($_ = $this->getOgProperty('og:description')) {
+      return $_;
     }
 
+    // We can't find a description, take the start of the text.
+    $text = substr($this->getMainText(), 0, 160);
+    $i = strrpos($text, '.');
+    if ($i===FALSE) {
+      // No full stops here, just don't split a word.
+      $i = strrpos($text, ' ');
+      if ($i>0) {
+        $text = substr($text, 0, $i) . '...';
+      }
+    }
+    else {
+      // Full stop found.
+      $text = substr($text, 0, $i) . '...';
+    }
+    return $text;
   }
   /**
    * Return the content attribute from a meta element with given property.
@@ -84,8 +138,23 @@ class HtmlScrape {
    * Works for og:image, for example.
    */
   public function getOgProperty($property) {
+    $this->requireContents();
 
     $nodes = $this->xpath->evaluate("//html/head/meta[@property=\"$property\"]");
+    if ($nodes->length > 0 && $nodes->item(0)->hasAttribute('content')) {
+      return $nodes->item(0)->getAttribute('content');
+    }
+
+  }
+  /**
+   * Return the content attribute from a meta element with given name.
+   *
+   * e.g. description.
+   */
+  public function getMetaByName($name) {
+    $this->requireContents();
+
+    $nodes = $this->xpath->evaluate("//html/head/meta[@name=\"$name\"]");
     if ($nodes->length > 0 && $nodes->item(0)->hasAttribute('content')) {
       return $nodes->item(0)->getAttribute('content');
     }
@@ -95,10 +164,113 @@ class HtmlScrape {
    * Helper.
    */
   public function getTextContentFromXPath($expression) {
+    $this->requireContents();
     $nodes = $this->xpath->evaluate($expression);
     if ($nodes->length > 0) {
       return $nodes->item(0)->textContent;
     }
+  }
+  /**
+   * Find main section and return plain text.
+   *
+   * All lines are trim()ed.
+   */
+  public function getMainText() {
+    $this->requireContents();
+    $node = $this->getMainSection();
+    $text = '';
+    foreach (explode("\n", $node->textContent) as $_) {
+      $text .= trim($_) . "\n";
+    }
+    return trim($text);
+  }
+  public function getFaviconUrl() {
+    $this->requireContents();
+    // check for <link rel="shortcut icon" href="url" >
+    $nodes = $this->xpath->evaluate("//html/head/link[@rel='shortcut icon']");
+    if ($nodes->length > 0 && $nodes->item(0)->hasAttribute('href')) {
+      return $nodes->item(0)->getAttribute('href');
+    }
+
+    // Default to domain/favion.ico.
+    $u = $this->splitUrl();
+    $_ = "{$u->protocol}{$u->domain}/favicon.ico";
+
+    return $_;
+  }
+  public function splitUrl() {
+    if (!preg_match('@^(https?://)([^/]+)((?:/[^?#]*)?)((?:[?][^#]*)?)((?:#.*)?)$@', $this->url, $matches)) {
+      throw new \InvalidArgumentException("URL '$this->url' could not be parsed.");
+    }
+    return (object)[
+      'protocol' => $matches[1],
+      'domain'   => $matches[2],
+      'path'     => $matches[3],
+      'query'    => $matches[4],
+      'fragment' => $matches[5],
+    ];
+  }
+  /**
+   * Ensures we have the content loaded and parsed.
+   */
+  protected function requireContents() {
+    if ($this->content_loaded) {
+      return;
+    }
+    // Content not loaded yet.
+    if (empty($this->url)) {
+      throw new \Exception("Cannot load content without URL.");
+    }
+    $this->raw = file_get_contents($this->url);
+    $this->parseRaw();
+    return $this;
+  }
+  /**
+   * Attempt to find the main content section.
+   *
+   * @return DOMElement
+   */
+  protected function getMainSection() {
+    if (isset($this->mainSectionNode)) {
+      return $this->mainSectionNode;
+    }
+
+    // Is there a #main-content?
+    $nodes = $this->xpath->evaluate("//*[@id='main-content']");
+    if ($nodes->length > 0) {
+      // This is probably an <a> tag.
+      if (strtolower($nodes->item(0)->tagName) == 'a') {
+        // Return the parent.
+        $this->mainSectionNode = $nodes->item(0)->parentNode;
+      }
+      else {
+        $this->mainSectionNode = $nodes->item(0);
+      }
+      return $this->mainSectionNode;
+    }
+
+    // HTML5 <main> tag?
+    $nodes = $this->xpath->evaluate("//main");
+    if ($nodes->length) {
+      $this->mainSectionNode = $nodes->item(0);
+      return $this->mainSectionNode;
+    }
+
+    // By default just return <body>
+    $this->mainSectionNode = $this->xpath->evaluate("//body")->item(0);
+    return $this->mainSectionNode;
+  }
+  /**
+   * Parses the HTML.
+   *
+   * Called immediately by loadFromString() and called by requireContents().
+   */
+  protected function parseRaw() {
+    $this->dom = new \DOMDocument();
+    libxml_use_internal_errors(TRUE);
+    $this->dom->loadHTML($this->raw);
+    $this->xpath = new \DOMXPath($this->dom);
+    $this->content_loaded = TRUE;
   }
 }
 
--- tests/HtmlScrapeTest.php
+++ tests/HtmlScrapeTest.php
@@ -3,9 +3,30 @@ use \ArtfulRobot\HtmlScrape;
 
 class HtmlScrapeTest extends \PHPUnit_Framework_TestCase {
   const FIXTURE_URL_1 = './tests/fixtures/scrape1.html';
+  const FIXTURE_URL_2 = './tests/fixtures/scrape2.html';
+  const FIXTURE_URL_3 = './tests/fixtures/scrape3.html';
 
-  public function testOgImage() {
+  public function testParseUrl() {
+    foreach ([
+      'http://example.com' => (object)['protocol'=>'http://', 'domain'=>'example.com', 'path'=>'', 'query'=>'', 'fragment'=>''],
+      'http://example.com/' => (object)['protocol'=>'http://', 'domain'=>'example.com', 'path'=>'/', 'query'=>'', 'fragment'=>''],
+      'http://example.com/some/path?some=query&foo=bar#here' => (object)['protocol'=>'http://', 'domain'=>'example.com', 'path'=>'/some/path', 'query'=>'?some=query&foo=bar', 'fragment'=>'#here'],
+    ] as $input=>$expected_output) {
+      $actual = HtmlScrape::factory($input)->splitUrl();
+      $this->assertEquals($expected_output, $actual);
+    }
+  }
+  public function testFavicon() {
+    $favicon = HtmlScrape::factory(self::FIXTURE_URL_1)
+      ->getFaviconUrl();
+    $this->assertEquals('http://example.com/some/favicon.ico', $favicon);
+  }
+  public function testImage() {
     $url = HtmlScrape::factory(self::FIXTURE_URL_1)
+      ->getImageUrl();
+    $this->assertEquals('http://example.com/image.jpg', $url);
+
+    $url = HtmlScrape::factory(self::FIXTURE_URL_2)
       ->getImageUrl();
     $this->assertEquals('http://example.com/image.jpg', $url);
   }
@@ -18,6 +39,25 @@ class HtmlScrapeTest extends \PHPUnit_Framework_TestCase {
     $_ = HtmlScrape::factory(self::FIXTURE_URL_1)
       ->getDescription();
     $this->assertEquals('This is the meta description.', $_);
+
+    $_ = HtmlScrape::factory(self::FIXTURE_URL_2)
+      ->getDescription();
+    $this->assertEquals('This is the og:description.', $_);
+
+    // Document without any description, should take the first bit of the main content.
+    $_ = HtmlScrape::factory(self::FIXTURE_URL_3)
+      ->getDescription();
+    $this->assertEquals("Test document\nThis document is all about socks.\nThere's a lot of information in here...",
+      $_);
+  }
+  public function testMain() {
+    $_ = HtmlScrape::factory(self::FIXTURE_URL_1)
+      ->getMainText();
+    $this->assertEquals("Test document\nThis document is all about socks.", $_);
+
+    $_ = HtmlScrape::factory(self::FIXTURE_URL_2)
+      ->getMainText();
+    $this->assertEquals("Test document\nScrape2 uses drupal style #main-content anchor", $_);
   }
 }
 
